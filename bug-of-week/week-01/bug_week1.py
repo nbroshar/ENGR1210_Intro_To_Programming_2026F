0669f83a5791145a30c570db57838a64e441676d
nbroshar <nbroshar@dunwoody.edu>
2026-06-05 09:20:43 -0500

    Baseline

---
+++ bug-of-week/week-01/bug_week1.py
@@ -0,0 +1,9 @@
+# bug_week1.py
+
+First Name = "jordan"
+last_name = 'Smith"
+age = "28"
+
+print("Name: " + first_name.title() + " " + Last_name.title())
+print("Age: " + age + 1)
+print("In 10 years, " + first_name + " will be " + age + 10 + " years old.")
